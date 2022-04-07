db300d6ac556a1c4ef75a1cfb04078b466d3c47d
mecaneer23 <mecaneer23@gmail.com>
2022-04-06 20:14:54 -0500

    fix argparse

--- snake.py
+++ snake.py
@@ -27,13 +27,16 @@ def main():
             return "Error in exiting"
         return exit_msg
 
-    parser = argparse.ArgumentParser(description="snake game")
-    parser.add_argument("--color", action="store_true", help="enable color mode")
-    parser.add_argument("-r", "--rows", type=int, help="set rows")
-    parser.add_argument("-c", "--columns", type=int, help="set columns")
-    parser.add_argument("--char-snake", type=int, help="set snake character")
-    parser.add_argument("--char-food", type=int, help="set food character")
-    parser.add_argument("--char-bg", type=int, help="set background character")
+    parser = argparse.ArgumentParser(description="snake game", add_help=False)
+    parser.add_argument("--help", "-h", action="help", help="show this help message and exit")
+    parser.add_argument("--cheat", type=int, help=argparse.SUPPRESS)
+    parser.add_argument("--black-white", "--bw", action="store_true", help="disable colors (black and white only)")
+    parser.add_argument("--rows", "-r", type=int, help="set rows")
+    parser.add_argument("--columns", "-c", type=int, help="set columns")
+    parser.add_argument("--char-snake", type=str, default="#", help="set snake character")
+    parser.add_argument("--char-food", type=str, default="*", help="set food character")
+    parser.add_argument("--char-bg", type=str, default=".", help="set background character")
+    parser.add_argument("--speed", "-s", type=int, default=10, help="set speed")
     args = parser.parse_args()
 
     stdscr = curses.initscr()
@@ -43,31 +46,26 @@ def main():
     curses.use_default_colors()
     curses.noecho()
     stdscr.nodelay(True)
-    stdscr.timeout(100)
+    stdscr.timeout(1000//args.speed)
     stdscr.keypad(1)
 
-    if args.color:
-        curses.init_pair(1, curses.COLOR_WHITE, -1)
-        curses.init_pair(2, curses.COLOR_GREEN, -1)
-        curses.init_pair(3, curses.COLOR_YELLOW, -1)
-    else:
-        curses.init_pair(1, -1, -1)
-        curses.init_pair(2, -1, -1)
-        curses.init_pair(3, -1, -1)
+    curses.init_pair(1, -1 if args.black_white else curses.COLOR_WHITE, -1)
+    curses.init_pair(2, -1 if args.black_white else curses.COLOR_GREEN, -1)
+    curses.init_pair(3, -1 if args.black_white else curses.COLOR_YELLOW, -1)
 
-    ROWS = stdscr.getmaxyx()[0] - 1 or args.rows
-    COLS = stdscr.getmaxyx()[1] - 1 or args.columns
-    CHAR_SNAKE = "#" or args.char_snake
-    CHAR_FOOD = "*" or args.char_food
-    CHAR_BG = "." or args.char_bg
+    ROWS = args.rows or stdscr.getmaxyx()[0] - 1
+    COLS = args.columns or stdscr.getmaxyx()[1] - 1
+    CHAR_SNAKE = args.char_snake
+    CHAR_FOOD = args.char_food
+    CHAR_BG = args.char_bg
     IS_LARGE_ENOUGH = COLS > 50
 
     snake = [[5, 5], [5, 4], [5, 3]]
-    score = 3
-    if len(sys.argv) > 1 and sys.argv[1].isdigit():
-        for i in range(int(sys.argv[1])):
+    score = len(snake)
+    if args.cheat:
+        for i in range(args.cheat):
             snake.append([5, 3])
-        score += int(sys.argv[1])
+        score += args.cheat
     food = [ROWS // 2, COLS // 2]
     direction = 100
     paused = False
